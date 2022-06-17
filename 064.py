a1d393f430442bf8a5937f4a26f869e7d809c247
edmfiedler <105977010+edmfiedler@users.noreply.github.com>
2022-06-17 18:51:37 +0200

    Update 064.py

--- 064.py
+++ 064.py
@@ -11,23 +11,18 @@ Find the number of odd period lengths for N <= 10,000
 o_count = 0
 for n in range(1,10000+1):
     sq = n**0.5
-    # Check if rational
     if int(sq)/sq == 1:
         continue
-    # Build sequence
-    not_found = True
-    x = sq
-    x_prev = sq
-    k = 1
-    while not_found:
-        y = x - int(x)
-        x = y**-1
-        if k == 1:
-            first = round(x,3)
-        elif round(x,3) == first:
-            if (k-1) % 2 == 1:
-                o_count += 1
-            #print(n,k-1)
-            not_found = False
-        k += 1
+    a = int(sq)
+    m = 0
+    d = 1
+    
+    count = 0
+    while int(sq)*2 != a:
+        m = d*a-m
+        d = (n-m**2)/d
+        a = int((int(sq)+m)/d)
+        count += 1
+    if count % 2 == 1:
+        o_count += 1
 print(o_count)
