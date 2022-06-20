3e06b8323098c5410391129bf0f62d4e1bc11fa1
edmfiedler <105977010+edmfiedler@users.noreply.github.com>
2022-06-20 21:55:26 +0200

    Update 070.py

--- 070.py
+++ 070.py
@@ -12,43 +12,28 @@ Find the value of n, 1 < n < 10**7, for which φ(n) is a permutation of n and th
 n/φ(n) produces a minimum.
 """
 
-max_n = 10**7
-p = [2]
-for n in range(3,max_n):
-    prime = 1
+min_t = 100
+n_max = 10000000
+for n in range(n_max,3,-1):
+    org = n
+    phi = n
     for x in range(2,int(n**0.5)+1):
         if n % x == 0:
-            prime = 0
-            break
-    
-    if prime == 1:
-        p.append(n)
-print("-----")
+            while n % x == 0:
+                n /= x
+            phi -= phi / x
 
-min_t = 100
-# Relative primes
-for n in range(2,max_n):
-    p_facs = []
-    for f in p:
-        if n % f == 0:
-            p_facs.append(f)
-        if f > n:
-            break
-    prod = 1
-    for pf in p_facs:
-        prod *= 1-(1/pf)
-    
-    phi = int(n*prod)
-    tot = n/phi
-    
-    list_n = sorted(list(set(str(n))))
-    count_n = [str(n).count(x) for x in list_n]
-    
-    list_phi = sorted(list(set(str(phi))))
-    count_phi = [str(phi).count(x) for x in list_phi]
-    
-    if list_n == list_phi and count_n == count_phi:
+    if n > 1:
+        phi -= phi / n
+
+    phi = int(phi)
+    tot = org/phi
+    list_n = sorted([x for x in str(org)])
+    list_phi = sorted([x for x in str(phi)])
+
+    if list_n == list_phi:
         if tot < min_t:
             min_t = tot
-            n_min = n
-            print(phi,n)
+            n_min = org
+            print(org,phi,tot)
+print(n_min)
